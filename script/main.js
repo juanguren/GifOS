1aa2b0c5d1c4279702820bcea1cdda7d743ef58b
Juan Felipe Aranguren <34801285+juanguren@users.noreply.github.com>
2020-03-07 21:28:21 -0500

    Toggle menu
    Worked on setting up classes through JS. Not really efficient but will stay with the result for now. Got to keep moving.

--- script/main.js
+++ script/main.js
@@ -1,8 +1,8 @@
 
-
 // Instatiating DOM elements
 let btnCreate = document.querySelector("#create-gif");
 let btnTheme = document.querySelector("#choose-theme");
+var toggleMenu = document.querySelector(".dropdown-content");
 
 // Event for "Crear GIFS"
 btnCreate.addEventListener("click", createGif);
@@ -14,8 +14,12 @@ function createGif() {
 // Event for "Elegir Tema"
 
 btnTheme.addEventListener("click", chooseTheme);
+toggleMenu.classList.add("not-show");
 
-function chooseTheme() {
-    console.log("2 clicked");
+function chooseTheme() {    
+    if (toggleMenu.classList.contains("not-show")) {
+        toggleMenu.classList.toggle("show");
+    }
     
 }
+
