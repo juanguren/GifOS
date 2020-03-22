6759b680f9ffec07636670b9ac66dc072a511603
Juan Felipe Aranguren <34801285+juanguren@users.noreply.github.com>
2020-03-21 20:22:02 -0500

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1,18 +1,23 @@
 
 // Instatiating DOM elements
+
+// SECTION TOOL BAR
 let btnCreate = document.querySelector("#create-gif");
 let btnTheme = document.querySelector("#choose-theme");
-var toggleMenu = document.querySelector(".dropdown-content");
+let toggleMenu = document.querySelector(".dropdown-content");
 
-// Event for "Crear GIFS"
+// SECTION SEARCH
+let searchBar = document.querySelector("#search-input");
+
+
+// Event for "Crear GIFS" (btnCreate)
 btnCreate.addEventListener("click", createGif);
 
 function createGif() {
     console.log("1 clicked");
 }
 
-// Event for "Elegir Tema"
-
+// Event for "Elegir Tema" (btnTheme & toggleMenu)
 btnTheme.addEventListener("click", chooseTheme);
 toggleMenu.classList.add("not-show");
 
@@ -20,6 +25,19 @@ function chooseTheme() {
     if (toggleMenu.classList.contains("not-show")) {
         toggleMenu.classList.toggle("show");
     }
-    
 }
+
+// Event for Buscar GIFS (searchBar)
+searchBar.addEventListener("click", searchClicked);
+
+function searchClicked(e) {
+    console.log(1);
+}
+
+searchBar.addEventListener("keyup", gifSearch);
+
+function gifSearch(e) {
+    console.log(e.target.value);
+}
+
 
