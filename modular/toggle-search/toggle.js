7a98ddb1bc636d0eb7b91a2253d75d8b897407b2
Juan Felipe Aranguren <34801285+juanguren@users.noreply.github.com>
2020-03-21 20:34:50 -0500

    Toggle module

--- modular/toggle-search/toggle.js
+++ modular/toggle-search/toggle.js
@@ -1,31 +1,9 @@
 
 // Instatiating DOM elements
 
-// SECTION TOOL BAR
-let btnCreate = document.querySelector("#create-gif");
-let btnTheme = document.querySelector("#choose-theme");
-let toggleMenu = document.querySelector(".dropdown-content");
-
 // SECTION SEARCH
 let searchBar = document.querySelector("#search-input");
 
-
-// Event for "Crear GIFS" (btnCreate)
-btnCreate.addEventListener("click", createGif);
-
-function createGif() {
-    console.log("1 clicked");
-}
-
-// Event for "Elegir Tema" (btnTheme & toggleMenu)
-btnTheme.addEventListener("click", chooseTheme);
-toggleMenu.classList.add("not-show");
-
-function chooseTheme() {    
-    if (toggleMenu.classList.contains("not-show")) {
-        toggleMenu.classList.toggle("show");
-    }
-}
 
 /* NOTE Events for GIF search */
 
