fe22307ac1c7ed116126459aa15949fd860454b4
Juan Felipe Aranguren <34801285+juanguren@users.noreply.github.com>
2020-03-25 13:20:43 -0500

    Toggle Achieved

--- modular/toggle-search/toggle.js
+++ modular/toggle-search/toggle.js
@@ -3,16 +3,33 @@
 
 // SECTION SEARCH
 let searchBar = document.querySelector("#search-input");
-
+let toggleBar = document.querySelector(".search-toggle"); /* NOTE NEW */
 
 /* NOTE Events for GIF search */
 
+/* NOTE NEW (START) */
+
+window.addEventListener("mouseup", (e) =>{
+    if (e.target != toggleBar && e.target.parentNode != toggleBar) {
+        toggleBar.style.display = "none";
+        searchToggleStatus = false;
+    }
+})
+
 // Toggle search menu
+
 searchBar.addEventListener("click", searchClicked);
 
+let searchToggleStatus = false; 
+
 function searchClicked(e) {
-    console.log(1);
+    if (!searchToggleStatus) {
+        toggleBar.style.display = "block";
+        searchToggleStatus = true;
+    } 
 }
+/* NOTE NEW (END) */
+
 
 // GIF search 
 searchBar.addEventListener("keyup", gifSearch);
