911b9557b8d37597063908374c78ea74e2a6a517
Juan Felipe Aranguren <34801285+juanguren@users.noreply.github.com>
2020-03-25 15:18:42 -0500

    Changed button
    The search button must change between inactive and active when clicking the search bar.
    It´s done.

--- script/main.js
+++ script/main.js
@@ -21,19 +21,24 @@ function chooseTheme() {
     }
 }
 
-
-
 // NOTE SEARCH 
 let searchBar = document.querySelector("#search-input");
 let toggleBar = document.querySelector(".search-toggle");
+let btnSearch = document.querySelector("#button-search");
+let glassIcon = document.querySelector("#lens-img");
 
 /* SECTION Events for GIF search toggle */
-
 let searchToggleStatus = false; 
 
 window.addEventListener("mouseup", (e) =>{
     if (e.target != toggleBar && e.target.parentNode != toggleBar) {
         toggleBar.style.display = "none";
+
+        btnSearch.classList.remove("btn-active");
+        btnSearch.classList.add("btn-inactive");
+
+        glassIcon.src = "assets/lupa_inactive.svg";
+
         searchToggleStatus = false;
     }
 })
@@ -43,8 +48,14 @@ searchBar.addEventListener("click", searchClicked);
 function searchClicked(e) {
     if (!searchToggleStatus) {
         toggleBar.style.display = "block";
+        
+        btnSearch.classList.remove("btn-inactive");
+        btnSearch.classList.add("btn-active");
+
+        glassIcon.src = "assets/lupa.svg";
+
         searchToggleStatus = true;
-    } 
+    }     
 }
 
 // SECTION GIF search 
